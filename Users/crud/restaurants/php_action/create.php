67c6537db536a7125e08d41799e817328cf7c105
jodhi <jodhilesmana@gmail.com>
2017-05-17 20:22:05 +0700

    done tambah restaurant

--- Users/crud/restaurants/php_action/create.php
+++ Users/crud/restaurants/php_action/create.php
@@ -2,28 +2,24 @@
 
 require_once '../../../../connect/db_connect.php';
 
-if($_POST)
-{
-	$restaurant_name = $_POST['restaurant_name'];
-	$restaurant_address = $_POST['restaurant_address'];
-	$restaurant_category = $_POST['restaurant_category'];
-	$restaurant_contact = $_POST['restaurant_contact'];
-	$restaurant_description = $_POST['restaurant_description'];
-	
-	$sql = "INSERT INTO restaurants (restaurant_id,menu_id,restaurant_name,restaurant_address,restaurant_category,restaurant_contact,restaurant_description,active) VALUES ('',-1,'$restaurant_name','$restaurant_address','$restaurant_category', '$restaurant_contact', '$restaurant_description',1)";
-	if($connect->query($sql) === TRUE)
-	{
-		echo "<p> New Restaurant Successfully Created, please wait for the approve</p>";
-		echo "<a href='../create.php'><button type='button'>Back</button></a>";
-		echo "<a href='../index.php'><button type='button'>Restaurant</button></a>";
-		echo "<a href='../../../home.php'><button type='button'>Home</button></a>";
-	}
-	else
-	{
-		echo "Error " . $sql . ' ' . $connect->connect_error;
-	}
-	
-	$connect->close();
-}
+if ($_POST) {
+    $restaurant_name = $_POST['restaurant_name'];
+    $restaurant_address = $_POST['restaurant_address'];
+    $restaurant_category = $_POST['restaurant_category'];
+    $restaurant_contact = $_POST['restaurant_contact'];
+    $restaurant_description = $_POST['restaurant_description'];
 
-?>
+    $sql = "INSERT INTO restaurants (restaurant_id,restaurant_name,restaurant_address,restaurant_category,restaurant_contact,restaurant_description,active) VALUES ('','$restaurant_name','$restaurant_address','$restaurant_category', '$restaurant_contact', '$restaurant_description',1)";
+
+    if ($connect->query($sql) === true) {
+        echo $sql;
+        echo "<p> New Restaurant Successfully Created, please wait for the approve</p>";
+        echo "<a href='../create.php'><button type='button'>Back</button></a>";
+        echo "<a href='../index.php'><button type='button'>Restaurant</button></a>";
+        echo "<a href='../../../home.php'><button type='button'>Home</button></a>";
+    } else {
+        echo "Error " . $sql . ' ' . $connect->connect_error;
+    }
+
+    $connect->close();
+}
--- api/tambah_restaurant.php
+++ api/tambah_restaurant.php
@@ -7,14 +7,15 @@ $json_input_data=json_decode(file_get_contents('php://input'), true);
     $restaurant_name =$json_input_data['restaurant_name'];
     $restaurant_address = $json_input_data['restaurant_address'];
     $restaurant_category = $json_input_data['restaurant_category'];
-    $restaurant_contact = $json_input_data['restaurant_contact'];
+    $restaurant_contact =  intval($json_input_data['restaurant_contact']);
     $restaurant_description = $json_input_data['restaurant_description'];
   //$image = $json_input_data['image'];
     //di insert tambahin image
-    $sql = "INSERT INTO restaurants (restaurant_id,menu_id,restaurant_name,restaurant_address,restaurant_category,restaurant_contact,restaurant_description,active) VALUES ('',-1,'$restaurant_name','$restaurant_address','$restaurant_category', '$restaurant_contact', '$restaurant_description',1)";
+    $sql = "INSERT INTO restaurants (restaurant_id,restaurant_name,restaurant_address,restaurant_category,restaurant_contact,restaurant_description,active) VALUES ('','$restaurant_name','$restaurant_address','$restaurant_category', '$restaurant_contact', '$restaurant_description',1)";
     if ($connect->query($sql) === true) {
         echo json_encode(array('message' => 'sukses'));
     } else {
+        // echo json_encode(array('message' => 'gagal'));
         echo "Error " . $sql . ' ' . $connect->connect_error;
     }
 
