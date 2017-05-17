8f1914d2184b502e1342c85016f1bd4d0a4e9aa8
jodhi <jodhilesmana@gmail.com>
2017-05-17 18:00:44 +0700

    tambah restaurant api

---
+++ api/tambah_restaurant.php
@@ -0,0 +1,21 @@
+<?php
+include '../connect/db_connect.php';
+
+$json_input_data=json_decode(file_get_contents('php://input'), true);
+
+
+    $restaurant_name =$json_input_data['restaurant_name'];
+    $restaurant_address = $json_input_data['restaurant_address'];
+    $restaurant_category = $json_input_data['restaurant_category'];
+    $restaurant_contact = $json_input_data['restaurant_contact'];
+    $restaurant_description = $json_input_data['restaurant_description'];
+  //$image = $json_input_data['image'];
+    //di insert tambahin image
+    $sql = "INSERT INTO restaurants (restaurant_id,menu_id,restaurant_name,restaurant_address,restaurant_category,restaurant_contact,restaurant_description,active) VALUES ('',-1,'$restaurant_name','$restaurant_address','$restaurant_category', '$restaurant_contact', '$restaurant_description',1)";
+    if ($connect->query($sql) === true) {
+        echo json_encode(array('message' => 'sukses'));
+    } else {
+        echo "Error " . $sql . ' ' . $connect->connect_error;
+    }
+
+    $connect->close();
