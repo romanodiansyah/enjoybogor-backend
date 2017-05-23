983650d10ecbbc52369020c0ae22f3190b04752b
JODHI LESMANA PUTRA <jodhilesmana@gmail.com>
2017-05-23 18:40:47 +0700

    reload account page

---
+++ api/account.php
@@ -0,0 +1,33 @@
+<?php
+
+include '../connect/db_connect.php';
+
+
+
+$json_input_data=json_decode(file_get_contents('php://input'), true);
+$username=$json_input_data['username'];
+
+
+
+    $perintah = "select * from users WHERE username = '$username' ";
+    $hasil = mysqli_query($connect, $perintah);
+
+        //create an array
+        while ($row =mysqli_fetch_assoc($hasil)) {
+            $id = $row['user_id'];
+            $nama = $row['user_name'];
+            $email = $row['email'];
+            $contact= $row['user_contact'];
+            $points = $row['points'];
+        }
+    if ($nama==null & $email==null) {
+        echo json_encode(array('status' => false, 'message' => 'username or password error'));
+        return;
+    }
+
+
+    header('Content-type: application/json');
+    echo json_encode(array('status' => true, 'name' => $nama, 'id' => $id, 'email'=>$email, 'username'=>$username,'contact'=>$contact , 'points'=> $points ));
+
+
+  mysqli_close($connect);
