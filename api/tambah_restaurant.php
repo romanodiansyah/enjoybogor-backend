c56232d46f64a98a5fda07e16e4865c23d619d92
JODHI LESMANA PUTRA <jodhilesmana@gmail.com>
2017-06-01 07:25:23 +0700

    success implement maps

--- api/tambah_restaurant.php
+++ api/tambah_restaurant.php
@@ -9,9 +9,11 @@ $json_input_data=json_decode(file_get_contents('php://input'), true);
     $restaurant_category = $json_input_data['restaurant_category'];
     $restaurant_contact =  intval($json_input_data['restaurant_contact']);
     $restaurant_description = $json_input_data['restaurant_description'];
-  //$image = $json_input_data['image'];
+    $latitude = $json_input_data['latitude'];
+    $longitude = $json_input_data['longitude'];
+  // $image = $json_input_data['image'];
     //di insert tambahin image
-    $sql = "INSERT INTO restaurants (restaurant_id,restaurant_name,restaurant_address,restaurant_category,restaurant_contact,restaurant_description,active) VALUES ('','$restaurant_name','$restaurant_address','$restaurant_category', '$restaurant_contact', '$restaurant_description',1)";
+    $sql = "INSERT INTO restaurants (restaurant_id,restaurant_name,restaurant_address,restaurant_category,restaurant_contact,restaurant_description,active,lat,lng) VALUES ('','$restaurant_name','$restaurant_address','$restaurant_category', '$restaurant_contact', '$restaurant_description',1,'$latitude','$longitude')";
     if ($connect->query($sql) === true) {
         echo json_encode(array('message' => 'sukses'));
     } else {
